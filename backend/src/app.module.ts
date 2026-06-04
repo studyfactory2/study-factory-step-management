cb6d7f272ca8e06e5c86d7957e58c14fa9df0656
hwan2 <hwan2@hwan2ui-Macmini.local>
2026-06-04 14:55:41 +0900

    chore: add jwt auth configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,14 @@ import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 import { appConfig } from "./config/app.config";
 import { databaseConfig } from "./config/database.config";
+import { jwtConfig } from "./config/jwt.config";
 import { MemberModule } from "./member/member.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, databaseConfig]
+      load: [appConfig, databaseConfig, jwtConfig]
     }),
     AuthModule,
     MemberModule
